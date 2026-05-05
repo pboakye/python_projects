d09fb79fe3f2e27c5a01dff1e9884c89c604e3a4
PRINCE <pboakye2005@gmail.com>
2026-05-05 23:35:55 +0000

    uploading my code

---
+++ operating system.py
@@ -0,0 +1,11 @@
+import os
+system_files = r"C:\Users\PRINCE\Downloads"
+files = os.listdir(system_files)
+print(files)
+for file in files:
+    print(f"checking files {files}")
+    os.path.splitext()
+    name,extension = os.path.splitext(file)
+    import shutil
+    docs_folder = os.path.join(system_files ,"Documents")
+
