15b767b290f6657942f6a815ed2ab6fc5cb3c4af
PRINCE <pboakye2005@gmail.com>
2026-05-07 02:05:45 +0000

    uploading my code

--- main.py
+++ main.py
@@ -16,3 +16,4 @@ while trial_attempt < 3:
 
 
 
+
--- operating system.py
+++ operating system.py
@@ -10,4 +10,19 @@ for file in files:
     docs_folder = os.path.join(system_files ,"Documents")
     if not os.path.exists(docs_folder):
         os.makedirs(docs_folder)
+        if extension == ".pdf":
+            source = os.path.join(system_files,file)
+            destination = os.path.join(docs_folder,file)
+            shutil.move(source,destination)
+            print(f"Moved {file} Successfully")
+            extensions_directory = {".pdf" : "Documents",
+                          ".jpg" : "Pictures",
+                          ".docx" :"Documents",
+                          ".xlsx" :"Documents",
+                          ".mp3" : "Music",
+                          ".png" : "Pictures"
+                          }
+            if extension in extensions_directory:
+                folder_name = extensions_directory[extension]
+
 
