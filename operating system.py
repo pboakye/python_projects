eb60ca39d727d263e689ad9d15a490b5dc4106aa
PRINCE <pboakye2005@gmail.com>
2026-05-14 17:13:03 +0000

    uploading my code

--- operating system.py
+++ operating system.py
@@ -1,28 +1,39 @@
 import os
-system_files = r"C:\Users\PRINCE\Downloads"
-files = os.listdir(system_files)
-print(files)
-for file in files:
-    print(f"checking files {files}")
-    os.path.splitext()
-    name,extension = os.path.splitext(file)
-    import shutil
-    docs_folder = os.path.join(system_files ,"Documents")
-    if not os.path.exists(docs_folder):
-        os.makedirs(docs_folder)
-        if extension == ".pdf":
-            source = os.path.join(system_files,file)
-            destination = os.path.join(docs_folder,file)
-            shutil.move(source,destination)
-            print(f"Moved {file} Successfully")
-            extensions_directory = {".pdf" : "Documents",
+import shutil
+from os import makedirs
+
+extensions_directory = {".pdf" : "Documents",
                           ".jpg" : "Pictures",
                           ".docx" :"Documents",
                           ".xlsx" :"Documents",
                           ".mp3" : "Music",
-                          ".png" : "Pictures"
+                          ".png" : "Pictures",
+                           ".mkv" : "Video"
                           }
-            if extension in extensions_directory:
-                folder_name = extensions_directory[extension]
+system_files = r"C:\Users\PRINCE\Desktop"
+files = os.listdir(system_files)
+for file in files:
+    print(f"checking files {files}")
+    os.path.splitext(file)
+    name,extension = os.path.splitext(file)
+    if extension in extensions_directory:
+        folder_name = extensions_directory[extension]
+        docs_folder = os.path.join(system_files, "Documents")
+        if not os.path.exists(docs_folder):
+            makedirs(docs_folder)
+        source = os.path.join(system_files, file)
+        destination = os.path.join(docs_folder, file)
+        shutil.move(source, destination)
+        print(f"Done! Moved {file} to {folder_name}")
+
+
+
+
+
+
+
+
+
+
 
 
