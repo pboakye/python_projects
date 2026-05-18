5dbf47cf362a7497132e872f0c099a24fbc16a34
PRINCE <pboakye2005@gmail.com>
2026-05-18 16:05:34 +0000

    uploading my code

--- operating system.py
+++ operating system.py
@@ -13,12 +13,12 @@ extensions_directory = {".pdf" : "Documents",
 system_files = r"C:\Users\PRINCE\Downloads"
 files = os.listdir(system_files)
 for file in files:
-    print(f"checking files {files}")
+    print(f"checking files {file}")
     os.path.splitext(file)
     name,extension = os.path.splitext(file)
     if extension in extensions_directory:
         folder_name = extensions_directory[extension]
-        docs_folder = os.path.join(system_files, "Documents")
+        docs_folder = os.path.join(system_files, folder_name)
         if not os.path.exists(docs_folder):
             makedirs(docs_folder)
         source = os.path.join(system_files, file)
